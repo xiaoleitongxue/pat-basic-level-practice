b8324f29b923ad1dbe477a62c86dc67c60587bcd
lilei <37171564+xiaoleitongxue@users.noreply.github.com>
2024-10-17 21:45:23 +0800

    调整项目结构

--- apps/pat1002/pat1002.cpp
+++ apps/pat1002/pat1002.cpp
@@ -3,18 +3,33 @@
 //
 
 #include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> dict = {"ling", "yi",  "er", "san", "si",
+                                 "wu",   "liu", "qi", "ba",  "jiu"};
 
 int main() {
-    int n;
-    std::cin >> n;
-    int count = 0;
-    while (n != 1) {
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else {
-            n = (3 * n + 1) / 2;
-        }
-        count++;
-    }
-    std::cout << count;
+  std::string input;
+  std::cin >> input;
+  long sum = 0;
+  for (const char c : input) {
+    sum += c - '0';
+  }
+
+  std::vector<std::string> pinyins;
+
+  while (sum != 0) {
+    int current_num = sum % 10;
+    sum /= 10;
+    pinyins.push_back(dict[current_num]);
+  }
+
+  std::cout << pinyins[pinyins.size() - 1];
+
+  for (int i = pinyins.size() - 2; i >= 0; --i) {
+    std::cout << " " << pinyins[i];
+  }
+  std::cout << std::endl;
 }
